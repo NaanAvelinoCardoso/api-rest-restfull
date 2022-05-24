0432eb65512b2961cdaceab0741f7bfe1c6b7bcf
NaanAvelinoCardoso <naanavelino2018@gmail.com>
2022-05-24 17:44:43 -0300

    Conectando ao Postgree

--- src/routers/api.ts
+++ src/routers/api.ts
@@ -5,9 +5,7 @@ import * as apiController from '../controllers/apiController';
 const router = Router();
 
 router.get('/ping', apiController.ping);
-
 router.get('/random', apiController.random);
-
 router.get('/name/:name', apiController.name);
 
 export default router;
